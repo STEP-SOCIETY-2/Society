e320a561532892c7f70b43c0f1d7e1da4bf947da
Okekeprince1 <okeke.p.ebube@gmail.com>
2021-12-24 16:23:15 +0100

    fix: changed share url

--- src/layouts/home/share/index.js
+++ src/layouts/home/share/index.js
@@ -27,7 +27,7 @@ export const Share = ({ activeSection, setActiveSection }) => {
 					<div className="flex flex-col lg:flex-row justify-center items-center lg:space-y-0 space-x-5 space-y-5">
 						<RWebShare
 							data={{
-								text: "Hey, Check this out!",
+								text: "Hey, Check this out! \n",
 								url: "http://joinsociety.org",
 								title: "Society",
 							}}
